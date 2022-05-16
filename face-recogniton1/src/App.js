155b6cf3240356014026f7b9060965be149fdd0d
andrea-everett <andrea.dawn.everett@gmail.com>
2022-05-15 22:26:53 -0400

    all

---
+++ face-recogniton1/src/App.js
@@ -0,0 +1,232 @@
+import React, { Component } from 'react';
+// import Particles from "react-tsparticles";
+// import{ loadFull } from "tsparticles";
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+import Navigation from './components/Navigation/Navigation';
+import Signin from './components/Signin/Signin';
+import Register from './components/Register/Register';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import './App.css';
+
+
+// const particlesLoaded = (container) => {
+//   console.log(container);
+// return (
+//   <Particles
+//     id="tsparticles"
+//     init={particlesInit}
+//     loaded={particlesLoaded}
+//     options={{
+//       background: {
+//         color: {
+//           value: "#0d47a1",
+//         },
+//       },
+//       fpsLimit: 120,
+//       interactivity: {
+//         events: {
+//           onClick: {
+//             enable: true,
+//             mode: "push",
+//           },
+//           onHover: {
+//             enable: true,
+//             mode: "repulse",
+//           },
+//           resize: true,
+//         },
+//         modes: {
+//           push: {
+//             quantity: 4,
+//           },
+//           repulse: {
+//             distance: 200,
+//             duration: 0.4,
+//           },
+//         },
+//       },
+//       particles: {
+//         color: {
+//           value: "#ffffff",
+//         },
+//         links: {
+//           color: "#ffffff",
+//           distance: 150,
+//           enable: true,
+//           opacity: 0.5,
+//           width: 1,
+//         },
+//         collisions: {
+//           enable: true,
+//         },
+//         move: {
+//           direction: "none",
+//           enable: true,
+//           outModes: {
+//             default: "bounce",
+//           },
+//           random: false,
+//           speed: 6,
+//           straight: false,
+//         },
+//         number: {
+//           density: {
+//             enable: true,
+//             area: 800,
+//           },
+//           value: 80,
+//         },
+//         opacity: {
+//           value: 0.5,
+//         },
+//         shape: {
+//           type: "circle",
+//         },
+//         size: {
+//           value: { min: 1, max: 5 },
+//         },
+//       },
+//       detectRetina: true,
+//     }}
+//   />
+// );
+
+
+  const initialState = {
+    input: '',
+    imageUrl: '',
+    box: {},
+    route: 'signin',
+    isSignedIn: false,
+    user: {
+      id: '',
+      name: '',
+      email: '',
+      password: '',
+      entries: 0,
+      joined: ''
+    }
+}
+
+// const particlesInit = async (main) => {
+//   console.log(main);
+
+//   // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
+//   // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+//   // starting from v2 you can add only the features you need reducing the bundle size
+//   await loadFull(main);
+// };
+
+  class App extends Component {
+    constructor () {
+      super();
+      this.state = initialState;
+      //let user = localStorage.getItem('AndreaAppUser');
+      //user = JSON.parse(user);
+  }
+
+  setUser = (data) => {
+  this.setState({user: {
+    id: data.id,
+    name: data.name,
+    email: data.email,
+    entries: data.entries,
+    joined: data.joined
+  }})
+  //develop local storage later
+  //localStorage.setItem('AndreaAppUser', JSON.stringify(data))
+}
+
+calculateFaceLocation = (data) => {
+  const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+  const image = document.getElementById('inputimage');
+  const width = Number(image.width);
+  const height = Number(image.height);
+  return {
+    leftCol: clarifaiFace.left_col * width,
+    topRow: clarifaiFace.top_row * height,
+    rightCol: width - (clarifaiFace.right_col * width),
+    bottomRow: height - (clarifaiFace.bottom_row * height),
+  }
+ }
+
+displayFaceBox = (box) => {
+  console.log('box', box);
+  this.setState({box: box});
+}
+
+onInputChange = (event) => {
+  this.setState({input: event.target.value});
+}
+
+  onButtonSubmit = () => {
+    this.setState({imageUrl: this.state.input});
+        fetch('http://localhost:3000/imageUrl', {
+          method: 'post',
+          headers: {'Content-Type' : 'application/json'},
+          body: JSON.stringify({
+            input: this.state.input
+          })
+    })
+        .then(response => response.json())
+        .then(response => {
+            console.log('response', response);
+            if (response) {
+              fetch('http://localhost:3000/image', {
+                method: 'put',
+                headers: {'Content-Type': "application/json"},
+                body: JSON.stringify({
+                  id: this.state.user.id
+                })
+        })
+        .then(response => response.json())
+        .then(count => {
+          return this.setState(Object.assign(this.state.user, { entries: count}))
+         })
+        .catch(console.log)
+      }
+      this.displayFaceBox(this.calculateFaceLocation(response))
+    })
+    .catch(err => console.log(err));
+  }
+
+  onRouteChange = (route) => {
+    if (route === 'signout') {
+      this.setState(initialState);
+    } else if (route === 'home') {
+      this.setState({isSignedIn:true});
+    }
+    this.setState({route: route});
+  }
+
+  render () {
+    const { isSignedIn, imageUrl, route, box } = this.state; 
+    return (
+      <div className="App">
+        <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange} />
+        { route === 'home'
+        ? <div>
+            <Logo />
+            <Rank name={this.state.user.name} 
+             entries={this.state.user.entries} 
+            />
+            <ImageLinkForm 
+              onInputChange={this.onInputChange}
+              onButtonSubmit={this.onButtonSubmit}
+            />
+            <FaceRecognition box={box} imageUrl={imageUrl} />
+          </div>
+          : (
+            route === 'Signin'
+            ? <Signin loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
+            : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
+          ) 
+        }
+    </div>
+   );
+  }
+}
+  
+export default App;
