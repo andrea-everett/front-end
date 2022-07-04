e7b15756439e74c19d6713ed13b2c5e7c6b3084e
andrea-everett <andrea.dawn.everett@gmail.com>
2022-07-04 17:36:41 -0400

    new-app

--- face-recogniton1/src/App.js
+++ face-recogniton1/src/App.js
@@ -30,20 +30,22 @@ class App extends Component {
   }
 
   loadUser = (data) => {
-    this.setState({user: {
-      id: data.id,
-      name: data.name,
-      email: data.email,
-      // password: data.password,
-      entries: data.entries,
-      joined: data.joined
-    }})
+    this.setState({
+      user: {
+        id: data.id,
+        name: data.name,
+        email: data.email,
+        // password: data.password,
+        entries: data.entries,
+        joined: data.joined
+      }
+    })
   }
 
   calculateFaceLocation = (data) => {
     const clarifaiFace = JSON.parse(data, null, 2).outputs[0].data.regions[0]
       .region_info.bounding_box;
-      console.log(clarifaiFace)
+    console.log(clarifaiFace)
     const image = document.getElementById("inputimage");
     const width = Number(image.width);
     const height = Number(image.height);
@@ -100,35 +102,35 @@ class App extends Component {
         body: raw,
       }
     )
-    .then((response) => response.text())
-    .then(response => {
-      if (response) {
-        fetch('https://red-celsius-13497.herokuapp.com/image', {
-          method: 'put',
-          headers: {'Content-Type': 'application/json'},
-          body: JSON.stringify({
-            id: this.state.user.id
+      .then((response) => response.text())
+      .then(response => {
+        if (response) {
+          fetch('https://scenic-bryce-canyon-04302.herokuapp.com/image', {
+            method: 'put',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+              id: this.state.user.id
+            })
           })
-        })
-        .then(response => {return response.json()})
-        .catch(err => console.log(err))
-        .then(count => {
-          this.setState(Object.assign(this.state.user, {entries: count}));
-        });
-      }
-      this.displayFaceBox(this.calculateFaceLocation(response));
-    })
-    .catch(error => console.log("error", error));
+            .then(response => { return response.json() })
+            .catch(err => console.log(err))
+            .then(count => {
+              this.setState(Object.assign(this.state.user, { entries: count }));
+            });
+        }
+        this.displayFaceBox(this.calculateFaceLocation(response));
+      })
+      .catch(error => console.log("error", error));
   };
 
 
   onRouteChange = (route) => {
-    if(route === 'signout'){
+    if (route === 'signout') {
       this.setState(initialState)
-    }else if (route === 'home'){
-      this.setState({isSignedIn: true})
+    } else if (route === 'home') {
+      this.setState({ isSignedIn: true })
     }
-    this.setState({route});
+    this.setState({ route });
   }
 
   render() {
@@ -137,21 +139,21 @@ class App extends Component {
       <div className="App">
         <Navigation isSignedIn={this.state.isSignedIn} onRouteChange={this.onRouteChange} />
         {this.state.route === 'home'
-         ? 
+          ?
           <div>
             <Logo />
             <Rank name={name} entries={entries} />
             <ImageLinkForm
-                  onInputChange={this.onInputChange}
-                  onButtonSubmit={this.onButtonSubmit}
-                  onEnter={this.onEnter}
+              onInputChange={this.onInputChange}
+              onButtonSubmit={this.onButtonSubmit}
+              onEnter={this.onEnter}
             />
             <FaceRecognition box={this.state.box} imageUrl={this.state.input} />
           </div>
           : (
             this.state.route === 'signin'
-            ? <SignIn onRouteChange={this.onRouteChange} loadUser={this.loadUser} />
-            : <Register onRouteChange={this.onRouteChange} loadUser={this.loadUser} />
+              ? <SignIn onRouteChange={this.onRouteChange} loadUser={this.loadUser} />
+              : <Register onRouteChange={this.onRouteChange} loadUser={this.loadUser} />
           )
         }
       </div>
